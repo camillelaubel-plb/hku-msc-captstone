56111b95d2be813376ade3df88b2afe28929a924
billy-hf-leung <57084773+billy-hf-leung@users.noreply.github.com>
2024-05-30 09:50:46 +0800

    update

--- testing/testing.py
+++ testing/testing.py
@@ -4,9 +4,14 @@ def add_numbers(a, b):
     return a + b
 
 class TestAddNumbers(unittest.TestCase):
-    def test_add_numbers(self):
+    def test_positive_numbers(self):
         self.assertEqual(add_numbers(2, 3), 5)
-        self.assertEqual(add_numbers(-1, 1), 0)
+
+    def test_negative_numbers(self):
+        self.assertEqual(add_numbers(-2, -3), -5)
+
+    def test_mixed_numbers(self):
+        self.assertEqual(add_numbers(2, -3), -1)
 
 if __name__ == '__main__':
     unittest.main()
