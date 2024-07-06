a88a0b146ce080877d0b9b1026f4f8b20d3de7b4
Camille Laubel <camille.laubel@premialab.com>
2024-07-06 15:41:57 +0800

    Update main

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import os
+import re
+import time
+# import memory_profiler
+import numpy as np
 import google.generativeai as genai
 
 from flask import Flask, request, jsonify
@@ -18,51 +22,69 @@ def ask_ai():
         result = data.get("result", {})
         expected_output = result.get("expectedOutput")
 
+        # https://platform.openai.com/docs/guides/prompt-engineering/tactic-use-code-execution-to-perform-more-accurate-calculations-or-call-external-apis
+        python_code = f"```python\n{code}\n```"
+
         model = configure_model(api_key)
         chat = model.start_chat(history=[])
-
 
         if not api_key or not code or not input_type or not boundary or not expected_output:
             raise ValueError("Missing required fields in the input.")
 
         # Validate Python syntax
-        syntax_check = chat.send_message(f"Ignore indentation errors and missing imports and respond with 'Yes' or 'No'. Is this correct Python syntax?\n`{code}`")
-        print(syntax_check.text)
+        syntax_check = chat.send_message("Analyze the following Python code snippet and identify any potential syntax errors."
+                                         "Do not include indentation and missing imports errors.\n"
+                                         f"{python_code}\n"
+                                         "Your response should be a simple 'Yes' if no syntax errors or 'No' if there are syntax errors.")
         
         if "No" in syntax_check.text:
-            syntax_errors = chat.send_message(f"List the syntax errors in bullet points:\n{code}")
-            print(syntax_errors.text)
+            syntax_errors = chat.send_message("List the previous syntax errors in bullet points")
             raise ValueError("Invalid Python syntax:", syntax_errors.text)
         
         # Generate sample data
         sample_data_query = (
-            f"Your responses will be used within a python program. You should adapt them so that it can be read by the flask jsonify function.\n"  
-            f"Generate relevant testing data for the Python code:\n`{code}`\n"
-            f"The input data type should be {input_type} and the boundary should be {boundary}."
-            f"Your response should only be a python list (minimum 20 items) of numbered list with the generated data."
+            f"Generate diverse testing input data for a Python program that utilizes Flask's jsonify function."
+            f"Given the following Python code:\n{python_code}\n"
+            f"Following the provided input format type: {input_type}. With the boundary condition being: {boundary}"
+            f"The desired output response is a Flask-compatible JSON list containing at least 20 diverse items."
         )
-        sample_data_response = chat.send_message(sample_data_query)
-        sample_data = eval(sample_data_response.text.replace('`', '').replace('python', ''))
-        print(sample_data)
 
-        # Edge Case Handling: Generate edge cases for testing, not just typical data.
-        # Randomized Data: Use libraries like faker to generate realistic and varied sample data.
-        # Custom Data Patterns: Allow users to specify patterns for input data generation
+        try:
+            sample_data_response = chat.send_message(sample_data_query)
+            partitioned_text = sample_data_response.text.partition("```json")[2].partition("```")[0]
+            sample_data = eval(partitioned_text)
+        except AttributeError:
+            sample_data = []
 
         # Validate the output
-        validation_query = (
-            f"Run the generated testing data on the code:\n{code}\n"
-            f"Are the results as expected? The expected output is {expected_output}."
-            f"Display your response as text"
-        )
+        validation_result = ""
+        execution_time = 0
+        if (len(sample_data) > 0):
+            output = run_external_code(code, sample_data)
+            result = output["results"]
 
-        validation_response = chat.send_message(validation_query)
-        validation_result = validation_response.text
-        print(validation_result)
+            validation_query = (
+                f"Check each of the results in the following list and determine if they match the expected output:\n{result}\n"
+                f"The desired output response is a text, describing if the output is as expected."
+            )
+
+            execution_time = output["execution_time"]
+
+            try:
+                validation_response = chat.send_message(validation_query)
+                validation_result = validation_response.text.replace("\n", "").replace("*", "").strip()
+            except AttributeError:
+                validation_result = "Generated data validation failed."
 
         # Generate unit tests
-        # unit_test_query = f"Generate ready-to-run Python unit tests for the provided code, including comments and descriptions:\n{code}" # with comment
-        unit_test_query = f"Generate ready-to-run Python unit tests for the provided code, without comments and descriptions:\n{code}" # without comment
+        unit_test_query = (
+            f"2. Your task is to Generate 10 ready-to-run Python unit tests for the provided code, without comments and descriptions:\n{python_code}\n"
+            f"1. The test would be using assertNotEqual instead of assertEqual for the all of the test cases."
+            f"4. Make sure the comparison between expected and actual output is done using same data types."
+            f"3. Before each variation, I want you to think through what a good header would be and elaborate on your reasoning before you write out the unit test."
+            f"5. Generate the test as mentioned above."
+            )
+        
         unit_test_response = chat.send_message(unit_test_query)
         unit_tests =  unit_test_response.text
 
@@ -70,22 +92,61 @@ def ask_ai():
         # Removing "``` python" "\\n" etc. human readable description
         unit_tests = massageOutput(unit_tests)
 
-        print(unit_tests)
+        # Generate metrics
+        ## Execution time
+        # start_time = time.time()
+        # result = my_function(data)
+        # end_time = time.time()
 
-        # Parameterized Tests: Create parameterized unit tests to handle multiple input scenarios efficiently.
-        # Mocking and Patching: Include examples of using unittest.mock to handle external dependencies.
-        # Test Coverage: Analyze test coverage and suggest additional test cases to improve it.
+        # execution_time = end_time - start_time
 
-        ## Suggenstions
-        ## 2nd LLM call to evaluate the code and unit tests
-        suggestions = callGeminiLLMForEvaluation(api_key, code, unit_tests)
+        ## Memory usage
+        # memory_usage = memory_profiler.memory_usage()[0]
+
+        ## Code Coverage
+        # import coverage
+
+        # cov = coverage.Coverage()
+        # cov.start()
+
+        # # Your code here (including all functions you want to test)
+
+        # cov.stop()
+        # cov.report()  # Prints line and branch coverage statistics
+
+        # # Optionally, generate an HTML report
+        # cov.html_report(directory='coverage_report') 
+
+
+
+        # Generate code quality metrics, such as cyclomatic complexity and maintainability index.
+        # Analyze the code for potential security vulnerabilities and suggest improvements.
+        # Evaluate the code for performance bottlenecks and suggest optimizations.
+
+
+        # Generate suggestions
+
+
+        #TODO:
+        ## Billy
+        ### 1. Integrate the extracted function to run and validate each unit test
+        ### 2. extract the tests from llm output
+        ### 3. Most likely will need to reuse the string code extraction to run the unit tests
+        ### 4. Only output the passed unit tests
+
+        ## Sebastian
+        ### 1. Implement the rest of the metrics generation
+        ### 2. Implement the suggestions generation
+
 
         response = {
             "sample_data": sample_data,
             "validation_result": validation_result,
             "unit_tests": unit_tests,
-            "metrics": [],  # Metrics can be added here
-            "suggestions": suggestions,  # Suggestions can be added here
+            "metrics": {
+                "execution_time": execution_time
+            },
+            "suggestions": []
         }
 
         return jsonify(response)
@@ -105,14 +166,32 @@ def massageOutput(input: str):
 
     return input
 
-# Gemini LLM call
-def callGeminiLLMForEvaluation(apiKey: str, inputCode: str, unitTest: str):
-    model = configure_model(apiKey)
-    chat = model.start_chat()
+def extract_function_name(code):
+    function_name_match = re.search(r'def\s+(\w+)\s*\(', code)
+    if function_name_match:
+        return function_name_match.group(1)
+    else:
+        return None
+    
+def run_external_code(string_code, data):
+    function_name = extract_function_name(string_code)
 
-    prompt = f"Evaluate the following Python code and unit test in short sentences (with code coverage metrics):\n{inputCode}\n{unitTest}"
+    if function_name is None:
+        raise ValueError("Missing function name. i.e. def my_function(data):")
+    
+    # Execute the function definition
+    exec(string_code)
+        
+    # Call the function dynamically
+    start_time = time.time()
+    result = eval(f'{function_name}({data})')
+    end_time = time.time()
+    execution_time = end_time - start_time
 
-    return chat.send_message(prompt).text
+    output = {}
+    output["results"] = result
+    output["execution_time"] = execution_time
+    return output
 
 def configure_model(api_key: str):
     genai.configure(api_key=api_key)
